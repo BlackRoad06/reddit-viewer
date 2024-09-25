c00debd75ff36e4e72ec9b27c440f97faa9d92b2
Blackroad06Gitgit config --global user.name Blackroad06Gitgit config --global user.name Blackroad06Git <Priyanthan1990@gmail.com>
2024-09-26 00:03:55 +0100

    fixed issues

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React from 'react';
 import PostPreviews from "./features/postPreviews/PostPreviews"
 import Header from './components/Header';
-import InputField from './components/InputField';
+
 
 function App() {
   return (
--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import PostPreviewsReducer from "./features/postPreviews/postPreviewsSlice";
-import PostPreviews from "./features/postPreviews/PostPreviews";
+
 
 export default configureStore({
     reducer: {
